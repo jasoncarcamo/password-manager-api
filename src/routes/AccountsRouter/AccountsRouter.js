37a8b7b091dffcab150c24c5ecafd7ccbdd2059c
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-01-29 10:22:46 -0500

    Added patch and delete endpoints for accounts

--- src/routes/AccountsRouter/AccountsRouter.js
+++ src/routes/AccountsRouter/AccountsRouter.js
@@ -47,4 +47,60 @@ AccountsRouter
             })
     })
 
+AccountsRouter
+    .route("/accounts/:id")
+    .all(express.json())
+    .all(express.urlencoded({ extended: true}))
+    .all(requireAuth)
+    .patch((req, res)=>{
+        const {
+            url,
+            email_used,
+            user_name,
+            password
+        } = req.body;
+
+        const editAccount = {
+            url,
+            email_used,
+            user_name,
+            password,
+            user_id: req.user.id
+        };
+
+        for(const [key, value] of Object.entries(editAccount)){
+            if(value == undefined){
+                return res.status(400).json({ error: `Missing ${key} in body request`});
+            };
+        };
+
+        AccountService.getUserAccount( req.app.get("db"), req.params.id)
+            .then( account => {
+                if(account.length == 0){
+                    
+                    return res.status(400).json({ error: "The account you are trying to edit does not exist"});
+                };
+
+                AccountService.updateAccount( req.app.get("db"), req.params.id, editAccount)
+                    .then( editedAccount =>{
+
+                        return res.status(200).json({ account: editedAccount})
+                    })
+            })
+    })
+    .delete((req, res)=>{
+        AccountService.getUserAccount( req.app.get("db"), req.params.id)
+            .then( exists => {
+                if(!exists){
+                    return res.status(400).json({ error: "The account does not exist"})
+                };
+
+                AccountService.deleteAccount( req.app.get("db"), req.params.id)
+                    .then( deleted => {
+
+                        return res.status(200).json({ success: "Account has been deleted"});
+                    })
+            })
+    })
+
 module.exports = AccountsRouter;
