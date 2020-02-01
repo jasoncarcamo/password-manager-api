f76c25be73469d8189f05b65e985468565d164e8
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-02-01 17:42:50 -0500

    Changed pocess.env.POST to process.env.PORT

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    PORT: process.env.POST || 8000,
+    PORT: process.env.PORT || 8000,
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://jason:carcamo11@localhost/password-manager-test",
     JWT_SECRET: process.env.JWT_SECRET  || "knwenfkwevjhwv",
     NODE_ENV: process.env.NODE_ENV || 'development',   
